2f4242a1ec569ad0553e558f1d831b72d89f03d8
Tomohiko <tabularasa066a@gmail.com>
2019-07-24 05:03:14 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
    protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello, world!"
+  def about
+
+  end
+
+  def home
+
+  end
+
+  def help
+
   end
 end
