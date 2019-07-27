06b7207de7be3fbe13a2d7019add31cc48adcefc
Tomohiko <tabularasa066a@gmail.com>
2019-07-27 17:38:00 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
-   protect_from_forgery with: :exception
-
-  def about
-
-  end
-
-  def home
-
-  end
-
-  def help
-
-  end
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
