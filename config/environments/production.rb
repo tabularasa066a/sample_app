688e35b16978e2101623dc04a20ed7b8a52a2d60
Tomohiko <tabularasa066a@gmail.com>
2019-07-29 23:48:38 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'floating-bayou-69926.herokuapp.com'　　　　　　　　　　#ここを自分のHerokuアプリのurlに変えることを忘れずに
+  host = 'https://floating-bayou-69926.herokuapp.com'　　　　　　　　　　#ここを自分のHerokuアプリのurlに変えることを忘れずに
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
